0efb7c5ba898d95f4894a5abc89e73ec8dab5977
David Weisdorf <dweisdorf@dephy.com>
2018-08-06 16:56:20 -0400

    submod update + current control gains changed

--- Python/flexseapython/flexsea_demo/currentcontrol.py
+++ Python/flexseapython/flexsea_demo/currentcontrol.py
@@ -19,31 +19,45 @@ varsToStream = [ 							\
 
 def fxCurrentControl(devId):
 
-	holdCurrent = 1000
+	holdCurrent = 500
 	fxSetStreamVariables(devId, varsToStream)
 	streamSuccess = fxStartStreaming(devId, 100, False, 0)
 
 	print('Setting controller to current...')
 	setControlMode(devId, CTRL_CURRENT)
-	setZGains(devId, 20, 1, 0, 0)
+	setZGains(devId, 100, 20, 0, 0)
 	setMotorCurrent(devId, holdCurrent) # Start the current, holdCurrent is in mA 
 
 	try:
 		while(True):
-			pass
+			sleep(0.1)
+			data = fxReadDevice(devId, varsToStream)
+			os.system('cls')
+			print("Holding Current: {} mA...".format(holdCurrent))
+			printData(labels, data)
+
 	except:
 		pass
 
 	print('Turning off current control...')
 	# ramp down first
-	n = 4
+	n = 50
 	for i in range(0, n):
 		setMotorCurrent(devId, holdCurrent * (n-i)/n)
+		sleep(0.04)
+
+	# wait for motor to spin down
+
+	setMotorCurrent(devId, 0)
+	lastAngle = fxReadDevice(devId, [FX_ENC_ANG])[0]
+	sleep(0.2)
+	currentAngle = fxReadDevice(devId, [FX_ENC_ANG])[0]
+	while( abs(currentAngle - lastAngle) > 100):
+		lastAngle = currentAngle
 		sleep(0.2)
+		currentAngle = fxReadDevice(devId, [FX_ENC_ANG])[0]
 
 	setControlMode(devId, CTRL_NONE)
-	# sleep so that the commmand makes it through before we stop streaming
-	sleep(0.2)
 	fxStopStreaming(devId)
 
 if __name__ == '__main__':
