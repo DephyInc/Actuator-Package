b4a268a61e469ae8bd26da5b00c1718dc204283d
dudds4 <weisdorf.david@gmail.com>
2019-01-28 17:08:42 +0000

    Forgot to commit a file

--- Python/flexseapython/pyFlexsea.py
+++ Python/flexseapython/pyFlexsea.py
@@ -73,8 +73,8 @@ def fxReadDevice(devId, fieldIds):
 	c_successBools, c_n = list_to_bool_arr( [0] * n )
 
 	result = flexsea.fxReadDevice(devId, c_fieldIds, c_successBools, c_n)
-
-	valsAsList = result[:n]
+        valsAsList = [result[i] for i in range(n)]
+        #valsAsList = result[:n]
 	boolsAsList = c_successBools[:n]
 
 	for i in range(0, n):
@@ -198,7 +198,7 @@ def loadFlexsea():
 def list_to_int_arr(l):
 	c_arr = (c_int * len(l))(*l)
 	c_len = c_int(len(l))
-	return c_arr, c_len
+        return c_arr, c_len
 
 def list_to_bool_arr(l):
 	c_arr = (c_bool * len(l))(*l)
