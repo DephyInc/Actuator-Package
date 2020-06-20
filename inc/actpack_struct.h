cb070e736b07fbbce911bb33c0845781857e05b4
Alia Layne <alialayne@hotmail.com>
2020-06-20 17:58:30 -0500

    adding new libs and modified ActPack struct

--- inc/actpack_struct.h
+++ inc/actpack_struct.h
@@ -12,10 +12,11 @@
  */
 
 #include "ActPack_device_spec.h"
-#include <stdio.h> 
-#include <time.h> 
-#include <string.h> 
-#include <stdint.h> 
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdbool.h>
 
 #define ACTPACK_SYSTEM_TIME_POS 32
 #define ACTPACK_STRUCT_DEVICE_FIELD_COUNT 33
@@ -33,41 +34,41 @@ extern "C"
 
 struct ActPackState
 {
-	int rigid;
-	int id;
-	int state_time;
-	int accelx;
-	int accely;
-	int accelz;
-	int gyrox;
-	int gyroy;
-	int gyroz;
-	int mot_ang;
-	int mot_vel;
-	int mot_acc;
-	int mot_cur;
-	int mot_volt;
-	int batt_volt;
-	int batt_curr;
-	int temperature;
-	int status_mn;
-	int status_ex;
-	int status_re;
-	int genvar_0;
-	int genvar_1;
-	int genvar_2;
-	int genvar_3;
-	int genvar_4;
-	int genvar_5;
-	int genvar_6;
-	int genvar_7;
-	int genvar_8;
-	int genvar_9;
-	int ank_ang;
-	int ank_vel;
-	//the system time
-	int systemTime;
-	uint32_t deviceData[ACTPACK_STRUCT_DEVICE_FIELD_COUNT];
+    int rigid;
+    int id;
+    int state_time;
+    int accelx;
+    int accely;
+    int accelz;
+    int gyrox;
+    int gyroy;
+    int gyroz;
+    int mot_ang;
+    int mot_vel;
+    int mot_acc;
+    int mot_cur;
+    int mot_volt;
+    int batt_volt;
+    int batt_curr;
+    int temperature;
+    int status_mn;
+    int status_ex;
+    int status_re;
+    int genvar_0;
+    int genvar_1;
+    int genvar_2;
+    int genvar_3;
+    int genvar_4;
+    int genvar_5;
+    int genvar_6;
+    int genvar_7;
+    int genvar_8;
+    int genvar_9;
+    int ank_ang;
+    int ank_vel;
+    //the system time
+    int systemTime;
+    //uint32_t deviceData[ACTPACK_STRUCT_DEVICE_FIELD_COUNT];
 };
 
 #pragma pack()
@@ -86,7 +87,7 @@ void ActPackSetData(struct ActPackState *actpack, uint32_t _deviceStateBuffer[],
 ///
 ///@param ActPack is the struct with the data to be placed in the string
 ///
-///@param dataString is where the new string wll be placed 
+///@param dataString is where the new string wll be placed
 ///
 void ActPackDataToString(struct ActPackState *actpack, char dataString[ACTPACK_DATA_STRING_LENGTH]);
 
@@ -96,12 +97,16 @@ void ActPackDataToString(struct ActPackState *actpack, char dataString[ACTPACK_D
 ///
 void ActPackGetLabels(char labels[ACTPACK_STRUCT_DEVICE_FIELD_COUNT][ACTPACK_LABEL_MAX_CHAR_LENGTH]);
 
-/// \brief retrieves the string equivalent of parameter names starting with state time.  Parameters 
-/// prior to state time, such as id,  are not included. 
+/// \brief retrieves the string equivalent of parameter names starting with state time.  Parameters
+/// prior to state time, such as id,  are not included.
 ///
 ///@param labels is the array of labels containing the parameter names
 ///
 int ActPackGetLabelsForLog(char labels[ACTPACK_STRUCT_DEVICE_FIELD_COUNT][ACTPACK_LABEL_MAX_CHAR_LENGTH]);
+
+void ActPackStructToDataArray(struct ActPackState actpack, int32_t actPackDataArray[ACTPACK_STRUCT_DEVICE_FIELD_COUNT]);
+
+int GetActPackDataByDataPosition(struct ActPackState actpack, int dataPosition, bool *dataValid);
 
 #ifdef __cplusplus
 }//extern "C"
