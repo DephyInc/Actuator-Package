63df855de8c050650fcd4b6f085e23c3f12074db
Arko Basu <abasu@dephy,com>
2020-07-30 03:04:04 -0500

    Validated c and py files from provided directory path that is to be compared. Filtered through the ones we need.

--- Python/fxStructsValidatePy-C.py
+++ Python/fxStructsValidatePy-C.py
@@ -2,13 +2,30 @@ from signal import signal, SIGINT
 import os
 import sys
 
+C_STRUCTS_DIR = os.path.join(os.getcwd(),"..","inc")
+PYTHON_DIR = os.path.join(os.getcwd(),"flexseapython", "dev_spec")
+IGNORE_FILES = ['__init__.py', '__pycache__', 'AllDevices.py']
+
 def sig_handler(frame, signal_received):
     return sys.exit('\nCTRL-C or SIGINT detected\nExiting ...')
 
 def get_py_structs():
+    #Filter only files that we are concerned with. Endswith: *State.py
+    all_py_files = [file for file in os.listdir(PYTHON_DIR)
+                    if file not in IGNORE_FILES
+                    if file.endswith("State.py")]
+    print ("\n>>> INFO: " + str(len(all_py_files)) + \
+           " Python State file(s) detected at " + PYTHON_DIR + ":")
+    print(*all_py_files, sep=", ")
     return
 
 def get_c_structs():
+    #Filter only files that we are concerned with. Endswith: *struct.h
+    all_c_files = [file for file in os.listdir(C_STRUCTS_DIR)
+                   if file.endswith("struct.h")]
+    print ("\n>>> INFO: " + str(len(all_c_files)) + \
+           " C struct file(s) detected at " + C_STRUCTS_DIR+ ":")
+    print(*all_c_files, sep=", ")
     return
 
 def get_matching_struct_files(python_struct_files, c_struct_files):
@@ -23,7 +40,6 @@ if __name__ == '__main__':
 
     print('\n>>> Actuator Package Python Demo Scripts : Validates Python and C structs.<<<'+\
           "\n>>> It only validates the fields of the structs")
-    list_of_structs_to_compare = []
 
     if len(sys.argv)!= 2:
         sys.exit("\n>>> ERR: Invalid arguments."+ \
@@ -32,20 +48,18 @@ if __name__ == '__main__':
                  "\n>>>        python fxStructsValidatePy-C.py BMS" )
 
     if sys.argv[1] == "all":
-        #Validate all files
-        #all_specs = next(os.walk())[2]
-        all_python_struct_files = get_py_structs()
-        all_c_struct_files = get_c_structs()
-        struct_files_w_matching_names = get_matching_struct_files(all_python_struct_files, all_c_struct_files)
-        print("\n>>> INFO: Validating struct file(s): " + struct_files_w_matching_names)
-        for struct_file in struct_files_w_matching_names:
-            validate_struct_files(struct_file)
+        all_python_files = get_py_structs()
+        all_c_files = get_c_structs()
+        files_w_matching_names = get_matching_struct_files(all_python_files, all_c_files)
+        """print("\n>>> INFO: Validating struct file(s): " + files_w_matching_names)
+        for struct_file in files_w_matching_names:
+            validate_struct_files(struct_file)"""
 
 
     else:
         #validate single struct file
         print("\n>>> INFO: Validating struct file(s): " + sys.argv[1])
-        validate_struct_files(argv[1])
+        #validate_struct_files(sys.argv[1])
 
 
 
