7f5ff9bf1cd796cfcbb7a3cfcd0bd728d7af9935
Carlos Asmat <casmat@dephy.com>
2021-01-26 19:38:21 -0500

    WIP more moving files around

--- Python/flexsea/setup.py
+++ Python/flexsea/setup.py
@@ -4,7 +4,7 @@ FlexSEA package setup info
 import setuptools
 
 with open("README.md", "r", encoding="utf-8") as fh:
-	long_description = fh.read()
+	LONG_DESCRIPTION = fh.read()
 
 setuptools.setup(
 	name="flexsea",  # Replace with your own username
@@ -12,10 +12,17 @@ setuptools.setup(
 	author="Dephy Inc.",
 	author_email="admin@dephy.com",
 	description="Dephy's Actuator Package API library",
-	long_description=long_description,
+	long_description=LONG_DESCRIPTION,
 	long_description_content_type="text/markdown",
 	url="https://github.com/DephyInc/Actuator-Package/",
 	packages=setuptools.find_packages(),
+	install_requires=[
+		"scipy==1.5.*",
+		"tornado==6.0.*",
+		"matplotlib==3.3.*",
+		"numpy==1.19.*",
+		"pillow==8.1.*",
+	],
 	classifiers=[
 		"Programming Language :: Python :: 3",
 		"License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
@@ -23,5 +30,6 @@ setuptools.setup(
 		"Operating System :: Microsoft :: Windows :: Windows 10",
 		"Development Status :: 5 - Production/Stable",
 	],
-	python_requires="=3.7",
+	python_requires="== 3.7.*",
+	package_data={"libs": ["../../libs"], "inc": ["../../inc"], "": ["README.md"],},
 )
