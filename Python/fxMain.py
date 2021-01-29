5da7981e22a9f7efc95cc199e4e3066ab1b47b38
Carlos Asmat <casmat@dephy.com>
2021-01-29 17:09:36 -0500

    adds legacy fxMain script

---
+++ Python/fxMain.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Legacy executable
+"""
+
+DICSLAIMER = """
+=====================================================================================
+This is a legacy executable file for those who are used to running fxMain.py.
+The contents of the original script have been moved to
+
+	flexsea_demo/run_demos.py
+
+Please use run_demos.py from the flexsea_demo folder.
+=====================================================================================
+"""
+
+if __name__ == "__main__":
+	print(DICSLAIMER)
