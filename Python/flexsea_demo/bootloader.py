95132d2759b672b21363aaa59b1af9dcd5fa8560
Tegin Mayadagli <tmayadagli@dephy.com>
2021-04-14 11:36:17 -0400

    Script will try once in every 5 seconds

--- Python/flexsea_demo/bootloader.py
+++ Python/flexsea_demo/bootloader.py
@@ -35,9 +35,10 @@ def bootloader(fxs, port, baud_rate, target="Mn"):
 		print(f"Activating {targets[target]['name']} bootloader", flush=True)
 		sleep(1)
 		print("Sending signal to target device", flush=True)
-		fxs.activate_bootloader(dev_id, targets[target]["id"])
 		timeout = 60  # seconds
 		while timeout > 0:
+			if timeout%5 == 0:
+				fxs.activate_bootloader(dev_id, targets[target]["id"])
 			print(f"Waiting for response from target ({timeout}s)", flush=True)
 			sleep(1)
 			timeout -= 1
