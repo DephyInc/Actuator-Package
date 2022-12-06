7e60c67c3631a58cde6ae34eab6081aaa8a1a3ff
Jared <jcoughlin@dephy.com>
2022-12-06 18:12:23 -0500

    Adds 9.2.0 (disconnect safety).

--- flexsea/specs/api_spec.py
+++ flexsea/specs/api_spec.py
@@ -1,8 +1,10 @@
 from ._api_specs.v7_2_0 import v7_2_0
 from ._api_specs.v9_1_0 import v9_1_0
+from ._api_specs.v9_2_0 import v9_2_0
 
 
 apiSpec = {
     "7.2.0": v7_2_0,
     "9.1.0": v9_1_0,
+    "9.2.0": v9_2_0,
 }
