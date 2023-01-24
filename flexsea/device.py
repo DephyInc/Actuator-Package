194e8401d71369fb9c0f1d9139109f833792abca
Jared <jcoughlin@dephy.com>
2023-01-24 14:16:26 -0500

    Device now searches for port if not given one. Port and baud rate are keyword args.

--- flexsea/device.py
+++ flexsea/device.py
@@ -3,6 +3,7 @@ import setuptools.version as ver
 from . import config as cfg
 from .dephy_device import DephyDevice
 from .legacy_device import LegacyDevice
+from .utilities import find_port
 
 
 # ============================================
@@ -19,14 +20,17 @@ class Device(LegacyDevice):
     # -----
     def __new__(
         cls,
-        port: str,
-        baudRate: int,
+        port: str = "",
+        baudRate: int = cfg.baudRate,
         cLibVersion: str = cfg.LTS,
         logLevel: int = 4,
         loggingEnabled: bool = True,
     ) -> DephyDevice | LegacyDevice:
         inUse = ver.pkg_resources.parse_version(cLibVersion)
         cutoff = ver.pkg_resources.parse_version(cfg.legacyCutoff)
+
+        if not port:
+            port = find_port(baudRate, cLibVersion)
 
         if inUse < cutoff:
             return LegacyDevice(port, baudRate, cLibVersion, logLevel, loggingEnabled)
