efab1b7a4e7046b31077dcc758c885a8786c386f
Jared <jcoughlin@dephy.com>
2023-03-08 11:58:00 -0500

    Adds v10.1.2 api spec.

--- flexsea/specs/api_spec.py
+++ flexsea/specs/api_spec.py
@@ -4,6 +4,7 @@ from ._api_specs.v8_0_0 import v8_0_0
 from ._api_specs.v9_1_0 import v9_1_0
 from ._api_specs.v9_2_0 import v9_2_0
 from ._api_specs.v10_0_0 import v10_0_0
+from ._api_specs.v10_1_2 import v10_1_2
 
 apiSpec = {
     "7.2.0": v7_2_0,
@@ -12,4 +13,5 @@ apiSpec = {
     "9.1.0": v9_1_0,
     "9.2.0": v9_2_0,
     "10.0.0": v10_0_0,
+    "10.1.2": v10_1_2,
 }
