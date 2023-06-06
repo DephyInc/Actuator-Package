49a24b7bf294eccff9d7950c6ba2c2e65e719725
Jared <jcoughlin@dephy.com>
2023-06-06 17:22:58 -0400

    get_device_spec now uses s3_download.

--- flexsea/utilities/specs.py
+++ flexsea/utilities/specs.py
@@ -1,7 +1,7 @@
-from cloudpathlib import CloudPath
 import yaml
 from semantic_version import Version
 
+from flexsea.utilities.aws import s3_download
 import flexsea.utilities.constants as fxc
 
 
@@ -15,16 +15,8 @@ def get_device_spec(deviceName: str, firmwareVersion: Version) -> dict:
 
     if not deviceSpecFile.exists():
         deviceSpecFile.parent.mkdir(parents=True, exist_ok=True)
-        objName = f"s3://{fxc.dephyPublicFilesBucket}/{fxc.legacyDeviceSpecsDir}/"
-        objName += f"{firmwareVersion}/{deviceSpecFile.name}"
-        obj = CloudPath(objName)
-
-        # We do this because cloudpathlib doesn't raise an exception when downloading
-        # a file that doesn't exist
-        if not obj.is_file():
-            raise FileNotFoundError("Error: could not find `{deviceSpecFile.name}`.")
-
-        obj.download_to(deviceSpecFile)
+        objName = f"{fxc.legacyDeviceSpecsDir}/{firmwareVersion}/{deviceSpecFile.name}"
+        s3_download(objName, fxc.dephyPublicFilesBucket, deviceSpecFile, None)
 
     with open(deviceSpecFile, "r", encoding="utf-8") as fd:
         deviceSpec = yaml.safe_load(fd)
