aa2d80f254a8e7105fd09cb40b9b66ec315717bd
Jared <jcoughlin@dephy.com>
2023-07-27 12:52:28 -0400

    Ports install docs to rst. Adds theming scaffolding.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,10 +6,16 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+
+# For configuration of the pydata theme, see this example:
+# https://github.com/martinfleis/momepy/tree/main/docs
+
+import flexsea
+
 project = "flexsea"
 copyright = "2023, Dephy, Inc."
 author = "Dephy, Inc."
-release = "11.0.7"
+release = flexsea.__version__
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -19,6 +25,7 @@ extensions = [
     "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",
     "sphinx.ext.intersphinx",
+    "sphinx_copybutton",
 ]
 
 intersphinx_mapping = {
@@ -27,11 +34,30 @@ intersphinx_mapping = {
 
 templates_path = ["_templates"]
 exclude_patterns = []
+add_function_parentheses = False
+today_fmt = "%B %d, %Y"
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "pydata_sphinx_theme"
-html_static_path = ["_static"]
 html_logo = "_static/logo-d.svg"
+html_theme_options = {
+    "github_url": "https://github.com/DephyInc/Actuator-Package",
+    "collapse_navigation": True,
+    "pygment_light_style": "lovelace",
+    "pygment_dark_style": "nord",
+    "header_links_before_dropdown": 6,
+    "navbar_end": ["theme-switcher", "navbar-icon-links"],
+    "logo": {
+        "image_light": "logo-d.svg",
+        "image_dark": "logo-d.svg",
+    },
+}
+html_static_path = ["_static"]
+html_favicon = "_static/favicon-16x16.png"
+
+
+def setup(app):
+    app.add_css_file("custom.css")
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,20 +1,11 @@
-.. flexsea documentation master file, created by
-   sphinx-quickstart on Mon Jul 24 14:52:45 2023.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
+.. _flexsea_docs_mainpage:
 
-Welcome to flexsea's documentation!
-===================================
+FlexSEA Documentation
+=====================
 
 .. toctree::
-   :maxdepth: 2
-   :caption: Contents:
+    :maxdepth: 1
+    :caption: Contents
 
-
-
-Indices and tables
-==================
-
-* :ref:`genindex`
-* :ref:`modindex`
-* :ref:`search`
+    Installing <installing/index>
+    API Reference <api/index>
