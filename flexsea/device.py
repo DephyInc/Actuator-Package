082fcc38a57b740d381319a9efddd1e0fb4550c5
Jared Coughlin <30010597+jcoughlin11@users.noreply.github.com>
2023-11-30 11:43:19 -0500

    [FX-290] Fixes training state (#126)
    * Calls fxUpdateTrainingData before reading training mode to fix runtimeerror.
    
    * Adds the requires_device_not decorator.
    
    * Adds calls to update training data for each training method. Adds warning if they fail.
    
    * Bumps version.
    
    ---------
    
    Co-authored-by: Jared <jcoughlin@dephy.com>
    Co-authored-by: Carlos Asmat <483059+Sotilrac@users.noreply.github.com>

--- flexsea/device.py
+++ flexsea/device.py
@@ -8,7 +8,9 @@ from semantic_version import Version
 
 import flexsea.utilities.constants as fxc
 from flexsea.utilities.decorators import minimum_required_version
+from flexsea.utilities.decorators import requires_device_not
 from flexsea.utilities.decorators import requires_status
+from flexsea.utilities.decorators import training_warn
 from flexsea.utilities.decorators import validate
 from flexsea.utilities.firmware import decode_firmware
 from flexsea.utilities.firmware import validate_given_firmware_version
@@ -483,6 +485,7 @@ class Device:
         """
         # There is a bug either on the C side or in the firmware where,
         # sometimes, the gains aren't set, so we try multiple times
+        returnCode = self._FAILURE
         for _ in range(5):
             returnCode = self._clib.fxSetGains(self.id, kp, ki, kd, k, b, ff)
             sleep(0.001)
@@ -1108,6 +1111,7 @@ class Device:
     # num_utts
     # -----
     @property
+    @requires_device_not("actpack")
     @minimum_required_version("9.1.0")
     @requires_status("connected")
     def num_utts(self) -> int:
@@ -1127,6 +1131,7 @@ class Device:
     # -----
     # set_all_utts
     # -----
+    @requires_device_not("actpack")
     @minimum_required_version("9.1.0")
     @requires_status("connected")
     @validate
@@ -1168,6 +1173,7 @@ class Device:
     # -----
     # set_utt
     # -----
+    @requires_device_not("actpack")
     @minimum_required_version("9.1.0")
     @requires_status("connected")
     @validate
@@ -1209,6 +1215,7 @@ class Device:
     # -----
     # reset_utts
     # -----
+    @requires_device_not("actpack")
     @minimum_required_version("9.1.0")
     @requires_status("connected")
     @validate
@@ -1231,6 +1238,7 @@ class Device:
     # -----
     # save_utts
     # -----
+    @requires_device_not("actpack")
     @minimum_required_version("9.1.0")
     @requires_status("connected")
     @validate
@@ -1254,6 +1262,7 @@ class Device:
     # -----
     # read_utts
     # -----
+    @requires_device_not("actpack")
     @minimum_required_version("9.1.0")
     @requires_status("connected")
     def read_utts(self) -> List[int]:
@@ -1300,8 +1309,9 @@ class Device:
     # -----
     # start_training
     # -----
-    @requires_status("connected")
     @validate
+    @requires_device_not("actpack")
+    @requires_status("connected")
     def start_training(self) -> int:
         """
         Activates training mode.
@@ -1326,13 +1336,16 @@ class Device:
         Training mode is only available if the device is flashed with
         one of Dephy's controllers.
         """
+        self._update_training_data()
+        sleep(0.25)
         return self._clib.fxStartTraining(self.id)
 
     # -----
     # activate_single_user_mode
     # -----
-    @requires_status("connected")
     @validate
+    @requires_device_not("actpack")
+    @requires_status("connected")
     def activate_single_user_mode(self) -> int:
         """
         Puts the device into single-user mode.
@@ -1357,13 +1370,16 @@ class Device:
         Training mode is only available if the device is flashed with
         one of Dephy's controllers.
         """
+        self._update_training_data()
+        sleep(0.25)
         return self._clib.fxUseSavedTraining(self.id)
 
     # -----
     # activate_multi_user_mode
     # -----
-    @requires_status("connected")
     @validate
+    @requires_device_not("actpack")
+    @requires_status("connected")
     def activate_multi_user_mode(self) -> int:
         """
         Puts the device into multi-user mode.
@@ -1387,12 +1403,15 @@ class Device:
         Training mode is only available if the device is flashed with
         one of Dephy's controllers.
         """
+        self._update_training_data()
+        sleep(0.25)
         return self._clib.fxDoNotUseSaveTraining(self.id)
 
     # -----
     # remaining_training_steps
     # -----
     @property
+    @requires_device_not("actpack")
     @requires_status("connected")
     def remaining_training_steps(self) -> int:
         """
@@ -1423,6 +1442,7 @@ class Device:
     # -----
     # get_training_user_mode
     # -----
+    @requires_device_not("actpack")
     @requires_status("connected")
     def get_training_user_mode(self) -> str:
         """
@@ -1444,6 +1464,9 @@ class Device:
         Training mode is only available if the device is flashed with
         one of Dephy's controllers.
         """
+        self._update_training_data()
+        sleep(1)
+
         singleUserMode = c.c_bool()
 
         retCode = self._clib.fxIsUsingSavedTrainingData(
@@ -1460,6 +1483,7 @@ class Device:
     # -----
     # get_training_state
     # -----
+    @requires_device_not("actpack")
     @requires_status("connected")
     def get_training_state(self) -> str:
         """
@@ -1490,6 +1514,7 @@ class Device:
     # -----
     # _update_training_data
     # -----
+    @training_warn
     @validate
     def _update_training_data(self) -> None:
         return self._clib.fxUpdateTrainingData(self.id)
@@ -1507,7 +1532,7 @@ class Device:
         int
             The integer corresponding to a status code of success.
         """
-        return self._SUCCESS
+        return self._SUCCESS.value
 
     # -----
     # failure
@@ -1522,7 +1547,7 @@ class Device:
         int
             The integer corresponding to a status code of failure.
         """
-        return self._FAILURE
+        return self._FAILURE.value
 
     # -----
     # undefined
@@ -1538,7 +1563,7 @@ class Device:
         int
             The integer corresponding to a status code of undefined.
         """
-        return self._UNDEFINED
+        return self._UNDEFINED.value
 
     # -----
     # invalidParam
@@ -1553,7 +1578,7 @@ class Device:
         int
             The integer corresponding to a status code of invalidParam.
         """
-        return self._INVALID_PARAM
+        return self._INVALID_PARAM.value
 
     # -----
     # invalidDevice
@@ -1568,7 +1593,7 @@ class Device:
         int
             The integer corresponding to a status code of invalidDevice.
         """
-        return self._INVALID_DEVICE
+        return self._INVALID_DEVICE.value
 
     # -----
     # isLegacy
--- flexsea/utilities/decorators.py
+++ flexsea/utilities/decorators.py
@@ -6,6 +6,87 @@ from semantic_version import Version
 
 
 # ============================================
+#              training_warn
+# ============================================
+def training_warn(func: Callable) -> Any:
+    """
+    Before training information is available or training commands can
+    be sent, the device must finish its setup. The purpose of this
+    wrapper is to provide a warning to the user if their command fails
+    about why it probably failed and what they can do about it.
+
+    Parameters
+    ----------
+    func : Callable
+        The method being wrapped. Must be a training-specific method.
+
+    Raises
+    ------
+    RuntimeError
+        If the wrapped method does not return a SUCCESS status code.
+
+    Returns
+    -------
+    Callable
+        The wrapped method.
+    """
+
+    @wraps(func)
+    def training_warn_wrapper(*args, **kwargs) -> Any:
+        try:
+            # The validate decorator runs the actual method and raises
+            # a RuntimeError if the method was not successfull. This
+            # try-except construction requires that the training_warn
+            # decorator be used "on top" of validate
+            retCode = func(*args, **kwargs)
+        except RuntimeError as err:
+            msg = f"Error: {func.__name__} failed. For training-specific methods, "
+            msg += "1.) the device must be running off of battery power, 2.) "
+            msg += "a Dephy controller must be present on the device, 3.) the device "
+            msg += "must be ready to provide augmentation."
+            raise RuntimeError(msg) from err
+        return retCode
+
+    return training_warn_wrapper
+
+
+# ============================================
+#            requires_device_not
+# ============================================
+def requires_device_not(device: str) -> Callable:
+    """
+    Certain ``Device`` class methods only work on non-Actpack devices.
+    This meta-decorator ensures this.
+
+    Parameters
+    ----------
+    device: str
+        The type of device that the physical device **cannot** be.
+
+    Raises
+    ------
+    RuntimeError
+        If the physical device has type ``device``.
+
+    Returns
+    -------
+    Callable
+        The method being wrapped.
+    """
+
+    def not_device_decorator(func: Callable) -> Callable:
+        @wraps(func)
+        def not_device_wrapper(*args, **kwargs) -> Any:
+            if getattr(args[0], "_name") == device:
+                raise RuntimeError(f"Error: {func.__name__} does not work on {device}")
+            return func(*args, **kwargs)
+
+        return not_device_wrapper
+
+    return not_device_decorator
+
+
+# ============================================
 #              requires_status
 # ============================================
 def requires_status(status: str) -> Callable:
@@ -25,7 +106,7 @@ def requires_status(status: str) -> Callable:
     Returns
     -------
     Callable
-        The function being wrapped.
+        The method being wrapped.
     """
 
     def status_decorator(func: Callable) -> Callable:
@@ -50,17 +131,17 @@ def validate(func: Callable) -> Callable:
     Parameters
     ----------
     func : Callable
-        The function being wrapped.
+        The method being wrapped.
 
     Raises
     ------
     RuntimeError
-        If the wrapped function does not return a SUCCESS status code.
+        If the wrapped method does not return a SUCCESS status code.
 
     Returns
     -------
     Callable
-        The wrapped function.
+        The wrapped method.
     """
 
     @wraps(func)
@@ -91,7 +172,7 @@ def minimum_required_version(version: str) -> Callable:
     Raises
     ------
     RuntimeError
-        If the wrapped function is not a :py:class:`Device` method or
+        If the wrapped method is not a :py:class:`Device` method or
         if the given version is greater than the device's firmware
         version.
 
