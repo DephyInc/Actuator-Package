e9e7f1062dc60803c70a08ae35f8f2e293940b83
Carlos Asmat <casmat@dephy.com>
2023-12-04 13:25:08 -0500

    makes try/xfepts more localized

--- demos/demo5_lsl_streaming.py
+++ demos/demo5_lsl_streaming.py
@@ -30,7 +30,7 @@ def main():
         "--port", default="/dev/ttyACM0", help="Port to use (default: /dev/ttyACM0)"
     )
     parser.add_argument(
-        "--speed", type=int, default=100, help="Speed setting (default: 100)"
+        "--freq", type=int, default=100, help="Freqency setting in Hz (default: 100)"
     )
     parser.add_argument(
         "--api", default="12.0.0", help="Version number (default: 12.0.0)"
@@ -41,7 +41,7 @@ def main():
 
     # Accessing the arguments
     print(f"Port: {args.port}")
-    print(f"Speed: {args.speed}")
+    print(f"Frequency: {args.freq}")
     print(f"API Version: {args.api}")
 
     if "windows" == platform.system().lower():
@@ -52,46 +52,49 @@ def main():
 
     try:
         device = Device(port=args.port, firmwareVersion=args.api)
-        device.open()
-        device.start_streaming(args.speed)
-
-        # Get the data labels
-        print(len(device._fields))
-        labels = sorted(device.read().keys())
-
-        print(f"{len(labels)} labels to be streamed:")
-        print(labels)
-
-        # Create LSL Stream
-        info = StreamInfo(
-            f"{device.id:X}",
-            "Dephy",
-            len(labels),
-            float(args.speed),
-            "int64",
-            f"{uuid.uuid4()}",
-        )
-
-        chns = info.desc().append_child("channels")
-        for label in labels:
-            ch = chns.append_child("channel")
-            ch.append_child_value("label", label)
-        outlet = StreamOutlet(info, 64, 360)
-
-        sleep(0.5)
-        while True:
-            try:
-                data = device.read(allData=True)
-                for sample in data:
-                    if sample["System Time"] != 0:
-                        outlet.push_sample([sample[key] for key in sorted(sample)])
-            except AssertionError as err:
-                print(f"Problem reading: {err}")
-            sleep(0.1)
-
-        device.close()
     except RuntimeError as err:
-        print(f"There's a problem connecting to the device at {args.port}: {err}")
+        print(f"There's a problem connecting to the device at {args.port}: \n {err}")
+        return
+
+    device.open()
+    device.start_streaming(args.freq)
+
+    # Get the data labels
+    print(len(device._fields))
+    labels = sorted(device.read().keys())
+
+    print(f"{len(labels)} labels to be streamed:")
+    print(labels)
+
+    # Create LSL Stream
+    info = StreamInfo(
+        f"{device.id:X}",
+        "Dephy",
+        len(labels),
+        float(args.freq),
+        "int64",
+        f"{uuid.uuid4()}",
+    )
+
+    chns = info.desc().append_child("channels")
+    for label in labels:
+        ch = chns.append_child("channel")
+        ch.append_child_value("label", label)
+    outlet = StreamOutlet(info, 64, 360)
+
+    sleep(0.5)
+    while True:
+        try:
+            data = device.read(allData=True)
+        except AssertionError as err:
+            data = []
+            print(f"Problem reading: {err}")
+        for sample in data:
+            if sample["System Time"] != 0:  # Filter out invalid readings
+                outlet.push_sample([sample[key] for key in sorted(sample)])
+        sleep(0.1)
+
+    device.close()
 
 
 if __name__ == "__main__":
